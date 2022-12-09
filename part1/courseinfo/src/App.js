0b8a6b869ae54442b974f916c1ccd0c61bddea99
Jana Sefcikova <29643207+janasefcikova@users.noreply.github.com>
2022-12-09 22:50:06 +0100

    Exercise 1.1 - extract components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,28 @@
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => (
+    <>
+        <p>
+            {props.part1} {props.exercises1}
+        </p>
+        <p>
+            {props.part2} {props.exercises2}
+        </p>
+        <p>
+            {props.part3} {props.exercises3}
+        </p>
+    </>
+)
+
+const Total = (props) => (
+    <p>Number of exercises {props.exerciseCount}</p>
+)
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -7,21 +32,17 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-  return (
-      <div>
-        <h1>{course}</h1>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-      </div>
-  )
+    return (
+        <div>
+            <Header course={course}/>
+            <Content
+                part1={part1} exercises1={exercises1}
+                part2={part2} exercises2={exercises2}
+                part3={part3} exercises3={exercises3}
+            />
+            <Total exerciseCount={exercises1 + exercises2 + exercises3}/>
+        </div>
+    )
 }
 
 export default App

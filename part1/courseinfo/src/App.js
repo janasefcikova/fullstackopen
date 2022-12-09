5529fba4304dbee93c80ce850cd1d22a0027ea6f
janasefcikova <9643207+janasefcikova@users.noreply.github.com>
2022-12-10 00:33:07 +0100

    Part 1: Exercise 1.5 Refactor data, pass them all as one object

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -31,29 +31,32 @@ const Total = (props) => (
 
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
+    let exerciseCount = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
     return (
         <div>
-            <Header course={course}/>
+            <Header course={course.name}/>
             <Content
-                parts={parts}
+                parts={course.parts}
             />
-            <Total exerciseCount={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
+            <Total exerciseCount={exerciseCount}/>
         </div>
     )
 }

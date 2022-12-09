1ee468160db5e1b4c9b73243bff7007c023413b2
Jana Sefcikova <29643207+janasefcikova@users.noreply.github.com>
2022-12-09 22:54:28 +0100

    Exercise 1.2 - courseinfo - extract Part component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,16 +6,16 @@ const Header = (props) => {
 
 const Content = (props) => (
     <>
-        <p>
-            {props.part1} {props.exercises1}
-        </p>
-        <p>
-            {props.part2} {props.exercises2}
-        </p>
-        <p>
-            {props.part3} {props.exercises3}
-        </p>
+        <Part part={props.part1} exercises={props.exercises1}/>
+        <Part part={props.part2} exercises={props.exercises2}/>
+        <Part part={props.part3} exercises={props.exercises3}/>
     </>
+)
+
+const Part = (props) => (
+    <p>
+        {props.part} {props.exercises}
+    </p>
 )
 
 const Total = (props) => (

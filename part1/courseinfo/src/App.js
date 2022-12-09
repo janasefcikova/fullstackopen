9ad7ceec15e7c21628740f3284f823b810d1cef1
janasefcikova <9643207+janasefcikova@users.noreply.github.com>
2022-12-10 00:29:03 +0100

    Part 1: Exercise 1.4 Refactor data to use array

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,13 +4,16 @@ const Header = (props) => {
     )
 }
 
-const Content = (props) => (
-    <>
-        <Part part={props.part1}/>
-        <Part part={props.part2}/>
-        <Part part={props.part3}/>
-    </>
-)
+const Content = (props) => {
+    const parts = props.parts
+    return (
+        <>
+            <Part part={parts[0]}/>
+            <Part part={parts[1]}/>
+            <Part part={parts[2]}/>
+        </>
+    )
+}
 
 const Part = (props) => {
     const part = props.part;
@@ -29,29 +32,28 @@ const Total = (props) => (
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
-
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
     return (
         <div>
             <Header course={course}/>
             <Content
-                part1={part1}
-                part2={part2}
-                part3={part3}
+                parts={parts}
             />
-            <Total exerciseCount={part1.exercises + part2.exercises + part3.exercises}/>
+            <Total exerciseCount={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
         </div>
     )
 }
